31bce6411aa664678633bce389c0ff96a67d254f
tomby5475 <pushkareva.nataly@gmail.com>
2024-11-22 14:52:10 +0100

    mobile css component intro done

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,17 +1,23 @@
 import Image from "next/image"
 import styles from '../app/page.module.css'
 import vadym from "../app/public/images/vadym.png"
+import wave from "../app/public/images/wave.png"
 
 export default function Intro () {
     return (
         <div className={styles.intro}>
             <h1>Great design will save the world</h1>
-            <Image
-                className={styles.vadym_foto}
-                src={vadym}
-                alt="vadym"
-            />
             <article className={styles.vadym_about}>
+                <div className={styles.wave_icon}>
+                    <Image
+                        src={wave}
+                        alt="wave hand"
+                    />
+                </div>
+                <Image className={styles.vadym_foto}
+                        src={vadym}
+                        alt="vadym"
+                />
                 <h2 className={styles.intro_heading}>Hi,</h2>
                 <span className={styles.intro_name}>I'm Vadym Grin</span>
                 <p className={styles.intro_subheading}>That's why I</p>
