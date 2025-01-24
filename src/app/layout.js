2335fe3306270bd19d7b005551fa89b34eee91ae
tomby5475 <pushkareva.nataly@gmail.com>
2025-01-24 18:33:50 +0100

    social preview

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,14 +5,24 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
+        <title>Vadym Grin</title>
+        <meta property="og:title" content="Vadym Grin" />
+        <meta property="og:description" content="Design can save the world. Website manifesto by designer, mentor, and writer Vadym Grin." />
+        <meta property="og:image" content="/images/personal-social.png" />
+        <meta property="og:url" content="https://example.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Catamaran:wght@400;700&display=swap"
           rel="stylesheet"
         />
         <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" href="/favicon.png" />
+        
       </Head>
       <body>{children}</body>
     </html>
   );
 }
+
+
+
+
