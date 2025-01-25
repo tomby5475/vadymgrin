d4ee2053999f078dc489bb6b6d21ba61ee8b3f6a
tomby5475 <pushkareva.nataly@gmail.com>
2025-01-25 15:18:08 +0100

    add width and hight to the image for SM preview

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,22 +7,26 @@ export default function RootLayout({ children }) {
       <Head>
         <title>Vadym Grin</title>
         <meta property="og:title" content="Vadym Grin" />
-        <meta property="og:description" content="Design can save the world. Website manifesto by designer, mentor, and writer Vadym Grin." />
-        <meta property="og:image" content="https://vadymgrin.vercel.app/images/personal-social.png" />
+        <meta
+          property="og:description"
+          content="Design can save the world. Website manifesto by designer, mentor, and writer Vadym Grin."
+        />
+        <meta
+          property="og:image"
+          content="https://vadymgrin.vercel.app/images/personal-social.png"
+        />
         <meta property="og:url" content="https://vadymgrin.vercel.app/" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="Vadym Grin Social Preview" />
         <link
           href="https://fonts.googleapis.com/css2?family=Catamaran:wght@400;700&display=swap"
           rel="stylesheet"
         />
         <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" href="/favicon.png" />
-        
       </Head>
       <body>{children}</body>
     </html>
   );
 }
-
-
-
-
