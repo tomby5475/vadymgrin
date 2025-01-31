de58686d0805d34477ce5097083177132c457615
tomby5475 <pushkareva.nataly@gmail.com>
2025-01-31 11:55:46 +0100

    open graph metatags should work

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,27 @@
-import Head from "next/head";
 import "./globals.css";
+
+export const metadata = {
+  title: "Vadym Grin",
+  description: "Design can save the world. Website manifesto by designer, mentor, and writer Vadym Grin.",
+  openGraph: {
+    title: "Vadym Grin",
+    type: "website",
+    description: "Design can save the world. Website manifesto by designer, mentor, and writer Vadym Grin.",
+    url: "https://vadymgrin.vercel.app/",
+    images: [
+      {
+        url: "https://vadymgrin.vercel.app/images/personal-social.png",
+        width: 1200,
+        height: 630,
+        alt: "Vadym Grin Social Preview",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>Vadym Grin</title>
-        <meta property="og:title" content="Vadym Grin" />
-        <meta property="og:type" content="image" />
-        <meta property="og:description" content="Design can save the world. Website manifesto by designer, mentor, and writer Vadym Grin."/>
-        <meta property="og:image" content="/public/images/personal-social.png" />
-        <meta property="og:url" content="https://vadymgrin.vercel.app/" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:image:alt" content="Vadym Grin Social Preview" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Catamaran:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" href="/favicon.png" />
-      </Head>
       <body>{children}</body>
     </html>
   );
