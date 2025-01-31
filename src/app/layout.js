d92107f513e230a3cb88cd880b6f6cf0e8343a53
tomby5475 <pushkareva.nataly@gmail.com>
2025-01-31 13:31:51 +0100

    add hotjar script and favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata = {
@@ -22,7 +24,30 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <Head>
+        <title>Vadym Grin</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
+      <body>
+        {children}
+        <Script
+          id="hotjar"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:3442557,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
+      </body>
     </html>
   );
 }
